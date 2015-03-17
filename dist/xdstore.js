efafcad604bdc4f051e695474da9fb5535fdfba3
Mirko Grönroos <hermo@dmz.fi>
2015-03-17 14:41:52 +0200

    initial import

---
+++ dist/xdstore.js
@@ -0,0 +1,324 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.xdstore = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! store2 - v2.2.0 - 2015-02-02
+* Copyright (c) 2015 Nathan Bubna; Licensed MIT, GPL */
+;(function(window, define) {
+    var _ = {
+        version: "2.2.0",
+        areas: {},
+        apis: {},
+
+        // utilities
+        inherit: function(api, o) {
+            for (var p in api) {
+                if (!o.hasOwnProperty(p)) {
+                    o[p] = api[p];
+                }
+            }
+            return o;
+        },
+        stringify: function(d) {
+            return d === undefined || typeof d === "function" ? d+'' : JSON.stringify(d);
+        },
+        parse: function(s) {
+            // if it doesn't parse, return as is
+            try{ return JSON.parse(s); }catch(e){ return s; }
+        },
+
+        // extension hooks
+        fn: function(name, fn) {
+            _.storeAPI[name] = fn;
+            for (var api in _.apis) {
+                _.apis[api][name] = fn;
+            }
+        },
+        get: function(area, key){ return area.getItem(key); },
+        set: function(area, key, string){ area.setItem(key, string); },
+        remove: function(area, key){ area.removeItem(key); },
+        key: function(area, i){ return area.key(i); },
+        length: function(area){ return area.length; },
+        clear: function(area){ area.clear(); },
+
+        // core functions
+        Store: function(id, area, namespace) {
+            var store = _.inherit(_.storeAPI, function(key, data, overwrite) {
+                if (arguments.length === 0){ return store.getAll(); }
+                if (data !== undefined){ return store.set(key, data, overwrite); }
+                if (typeof key === "string"){ return store.get(key); }
+                if (!key){ return store.clear(); }
+                return store.setAll(key, data);// overwrite=data, data=key
+            });
+            store._id = id;
+            store._area = area || _.inherit(_.storageAPI, { items: {}, name: 'fake' });
+            store._ns = namespace || '';
+            if (!_.areas[id]) {
+                _.areas[id] = store._area;
+            }
+            if (!_.apis[store._ns+store._id]) {
+                _.apis[store._ns+store._id] = store;
+            }
+            return store;
+        },
+        storeAPI: {
+            // admin functions
+            area: function(id, area) {
+                var store = this[id];
+                if (!store || !store.area) {
+                    store = _.Store(id, area, this._ns);//new area-specific api in this namespace
+                    if (!this[id]){ this[id] = store; }
+                }
+                return store;
+            },
+            namespace: function(namespace, noSession) {
+                if (!namespace){
+                    return this._ns ? this._ns.substring(0,this._ns.length-1) : '';
+                }
+                var ns = namespace, store = this[ns];
+                if (!store || !store.namespace) {
+                    store = _.Store(this._id, this._area, this._ns+ns+'.');//new namespaced api
+                    if (!this[ns]){ this[ns] = store; }
+                    if (!noSession){ store.area('session', _.areas.session); }
+                }
+                return store;
+            },
+            isFake: function(){ return this._area.name === 'fake'; },
+            toString: function() {
+                return 'store'+(this._ns?'.'+this.namespace():'')+'['+this._id+']';
+            },
+
+            // storage functions
+            has: function(key) {
+                if (this._area.has) {
+                    return this._area.has(this._in(key));//extension hook
+                }
+                return !!(this._in(key) in this._area);
+            },
+            size: function(){ return this.keys().length; },
+            each: function(fn, and) {
+                for (var i=0, m=_.length(this._area); i<m; i++) {
+                    var key = this._out(_.key(this._area, i));
+                    if (key !== undefined) {
+                        if (fn.call(this, key, and || this.get(key)) === false) {
+                            break;
+                        }
+                    }
+                    if (m > _.length(this._area)) { m--; i--; }// in case of removeItem
+                }
+                return and || this;
+            },
+            keys: function() {
+                return this.each(function(k, list){ list.push(k); }, []);
+            },
+            get: function(key, alt) {
+                var s = _.get(this._area, this._in(key));
+                return s !== null ? _.parse(s) : alt || s;// support alt for easy default mgmt
+            },
+            getAll: function() {
+                return this.each(function(k, all){ all[k] = this.get(k); }, {});
+            },
+            set: function(key, data, overwrite) {
+                var d = this.get(key);
+                if (d != null && overwrite === false) {
+                    return data;
+                }
+                return _.set(this._area, this._in(key), _.stringify(data), overwrite) || d;
+            },
+            setAll: function(data, overwrite) {
+                var changed, val;
+                for (var key in data) {
+                    val = data[key];
+                    if (this.set(key, val, overwrite) !== val) {
+                        changed = true;
+                    }
+                }
+                return changed;
+            },
+            remove: function(key) {
+                var d = this.get(key);
+                _.remove(this._area, this._in(key));
+                return d;
+            },
+            clear: function() {
+                if (!this._ns) {
+                    _.clear(this._area);
+                } else {
+                    this.each(function(k){ _.remove(this._area, this._in(k)); }, 1);
+                }
+                return this;
+            },
+            clearAll: function() {
+                var area = this._area;
+                for (var id in _.areas) {
+                    if (_.areas.hasOwnProperty(id)) {
+                        this._area = _.areas[id];
+                        this.clear();
+                    }
+                }
+                this._area = area;
+                return this;
+            },
+
+            // internal use functions
+            _in: function(k) {
+                if (typeof k !== "string"){ k = _.stringify(k); }
+                return this._ns ? this._ns + k : k;
+            },
+            _out: function(k) {
+                return this._ns ?
+                    k && k.indexOf(this._ns) === 0 ?
+                        k.substring(this._ns.length) :
+                        undefined : // so each() knows to skip it
+                    k;
+            }
+        },// end _.storeAPI
+        storageAPI: {
+            length: 0,
+            has: function(k){ return this.items.hasOwnProperty(k); },
+            key: function(i) {
+                var c = 0;
+                for (var k in this.items){
+                    if (this.has(k) && i === c++) {
+                        return k;
+                    }
+                }
+            },
+            setItem: function(k, v) {
+                if (!this.has(k)) {
+                    this.length++;
+                }
+                this.items[k] = v;
+            },
+            removeItem: function(k) {
+                if (this.has(k)) {
+                    delete this.items[k];
+                    this.length--;
+                }
+            },
+            getItem: function(k){ return this.has(k) ? this.items[k] : null; },
+            clear: function(){ for (var k in this.list){ this.removeItem(k); } },
+            toString: function(){ return this.length+' items in '+this.name+'Storage'; }
+        }// end _.storageAPI
+    };
+
+    // setup the primary store fn
+    if (window.store){ _.conflict = window.store; }
+    var store =
+        // safely set this up (throws error in IE10/32bit mode for local files)
+        _.Store("local", (function(){try{ return localStorage; }catch(e){}})());
+    store.local = store;// for completeness
+    store._ = _;// for extenders and debuggers...
+    // safely setup store.session (throws exception in FF for file:/// urls)
+    store.area("session", (function(){try{ return sessionStorage; }catch(e){}})());
+
+    if (typeof define === 'function' && define.amd !== undefined) {
+        define(function () {
+            return store;
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = store;
+    } else {
+        window.store = store;
+    }
+
+})(window, window.define);
+
+},{}],2:[function(require,module,exports){
+var store = require('store2')
+
+function xdstore(userConfig) {
+  if (!userConfig.target) {
+    
+    return { set: function(){}, get: function(){} }
+  }
+
+  var config = {
+    target: userConfig.target,
+    onReady: userConfig.onReady || function(){},
+    namespace: userConfig.namespace || 'xdstore',
+    permission: userConfig.permission || '*',
+    instance: Math.random().toString(36).substr(2, 9),
+    messageId: 0
+  }
+
+  var methods = {
+    _set_: function(options, callback) {
+      store.set(options.key, options.value)
+      callback(null)
+    },
+    _get_: function(options, callback) {
+      callback(null, store.get(options.key))
+    }
+  }
+
+  var callbacks = {}
+
+  function nextMessageId() {
+    return config.instance+'-'+(config.messageId+++1)
+  }
+
+  function sendMessage(name, data, id) {
+    config.target.postMessage({
+      namespace: config.namespace,
+      id: id || nextMessageId(),
+      name: name,
+      data: data
+    }, config.permission)
+  }
+
+  function onMessage(event) {
+    if (event.origin !== '*' && event.origin !== 'null' && event.origin !== config.permission) {
+      return 
+    }
+
+    var request = event.data
+    if (request.namespace !== config.namespace) { return }
+
+    if (request.name === '_init_') {
+      if (request.data === 'PING') {
+        sendMessage('_init_', 'PONG')
+      }
+
+      return config.onReady()
+    }
+
+    if (methods[request.name]) {
+      methods[request.name](request.data, function(error, data) {
+        sendMessage('_callback_'+request.name, {error: error, data: data}, request.id)
+      })
+    }
+
+    if (callbacks[request.id]) {
+      callbacks[request.id](request.data.error, request.data.data)
+      delete callbacks[request.id]
+    }
+  }
+
+  window.addEventListener('message', onMessage, false)
+
+  setTimeout(function() {
+    sendMessage('_init_', 'PING')
+  }, 0)
+
+  return {
+    set: function(key, value, callback) {
+      var messageId = nextMessageId()
+
+      if (callback) {
+        callbacks[messageId] = callback
+      }
+      sendMessage('_set_', {key: key, value: value}, messageId)
+    },
+    get: function(key, callback) {
+      var messageId = nextMessageId()
+
+      if (callback) {
+        callbacks[messageId] = callback
+      }
+      sendMessage('_get_', {key: key}, messageId)
+    }
+  }
+}
+
+module.exports = xdstore
+
+},{"store2":1}]},{},[2])(2)
+});
---
+++ src/xdstore.js
@@ -0,0 +1,97 @@
+var store = require('store2')
+
+function xdstore(userConfig) {
+  if (!userConfig.target) {
+    console.warn('[xdstore] No target window found!')
+    return { set: function(){}, get: function(){} }
+  }
+
+  var config = {
+    target: userConfig.target,
+    onReady: userConfig.onReady || function(){},
+    namespace: userConfig.namespace || 'xdstore',
+    permission: userConfig.permission || '*',
+    instance: Math.random().toString(36).substr(2, 9),
+    messageId: 0
+  }
+
+  var methods = {
+    _set_: function(options, callback) {
+      store.set(options.key, options.value)
+      callback(null)
+    },
+    _get_: function(options, callback) {
+      callback(null, store.get(options.key))
+    }
+  }
+
+  var callbacks = {}
+
+  function nextMessageId() {
+    return config.instance + '-' + (config.messageId+++1)
+  }
+
+  function sendMessage(name, data, id) {
+    config.target.postMessage({
+      namespace: config.namespace,
+      id: id || nextMessageId(),
+      name: name,
+      data: data
+    }, config.permission)
+  }
+
+  function onMessage(event) {
+    if (event.origin !== '*' && event.origin !== 'null' && event.origin !== config.permission) {
+      return console.warn('[xdstore] origin mismatch', 'origin=', event.origin,  'expected=', config.permission)
+    }
+
+    var request = event.data
+    if (request.namespace !== config.namespace) { return }
+
+    if (request.name === '_init_') {
+      if (request.data === 'PING') {
+        sendMessage('_init_', 'PONG')
+      }
+
+      return config.onReady()
+    }
+
+    if (methods[request.name]) {
+      methods[request.name](request.data, function(error, data) {
+        sendMessage('_callback_' + request.name, {error: error, data: data}, request.id)
+      })
+    }
+
+    if (callbacks[request.id]) {
+      callbacks[request.id](request.data.error, request.data.data)
+      delete callbacks[request.id]
+    }
+  }
+
+  window.addEventListener('message', onMessage, false)
+
+  setTimeout(function() {
+    sendMessage('_init_', 'PING')
+  }, 0)
+
+  return {
+    set: function(key, value, callback) {
+      var messageId = nextMessageId()
+
+      if (callback) {
+        callbacks[messageId] = callback
+      }
+      sendMessage('_set_', {key: key, value: value}, messageId)
+    },
+    get: function(key, callback) {
+      var messageId = nextMessageId()
+
+      if (callback) {
+        callbacks[messageId] = callback
+      }
+      sendMessage('_get_', {key: key}, messageId)
+    }
+  }
+}
+
+module.exports = xdstore
