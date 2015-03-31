7cdfabbbca598e729b4bb83e2f02b917a75ceea2
Mirko Grönroos <hermo@dmz.fi>
2015-03-31 13:46:18 +0300

    Disable Safari browser tests for now
    Saucelabs is not playing nice.

--- tests/intern-sauce.js
+++ tests/intern-sauce.js
@@ -9,18 +9,16 @@ define({
   environments: [
     { browserName: 'chrome', platform: [ 'Linux', 'OS X 10.10', 'Windows 7' ] },
     { browserName: 'firefox', platform: [ 'Linux', 'OS X 10.10', 'Windows 7' ] },
-    { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-    { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-    { browserName: 'internet explorer', version: '8' },
-    { browserName: 'safari', version: '6', platform: 'OS X 10.8' },
-    { browserName: 'safari', version: '7', platform: 'OS X 10.9' },
-    { browserName: 'safari', version: '8', platform: 'OS X 10.10' }
+    { browserName: 'internet explorer', version: ['8','9','10'] }
+    //{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+    //{ browserName: 'safari', version: '7', platform: 'OS X 10.9' },
+    //{ browserName: 'safari', version: '8', platform: 'OS X 10.10' }
   ],
 
   maxConcurrency: 3,
   tunnel: 'SauceLabsTunnel',
 
-  reporters: [ 'console' ],
+  reporters: [ 'pretty' ],
   functionalSuites: [ 'tests/functional/xdstore' ],
 
   excludeInstrumentation: /./
