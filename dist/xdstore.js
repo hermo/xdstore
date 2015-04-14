e945f88f8c5e32b757bc5247fd291ecdd1a38472
Mirko Grönroos <hermo@dmz.fi>
2015-04-14 11:28:32 +0300

    Update build

--- dist/xdstore.js
+++ dist/xdstore.js
@@ -292,6 +292,12 @@ var store = require('store2')
 
 // Check that localStorage really works on the current browser
 var localStorageWorks = (function() {
+  // Safari / MobileSafari, by default, seem to block localStorage even
+  // when used with an iframe from a different host so just give up and use
+  // cookies instead.
+  if (navigator.userAgent.indexOf('Safari') !== -1) {
+    return false;
+  }
   try {
     store.set('_sane', 1)
     if (store.get('_sane') === 1) {
